79e7b278aa2a6da6bca4f6484136b46b5fe23ac1
Chris Monson <shiblon@gmail.com>
2018-08-29 16:11:29 -0400

    Moved testing into qsvc library. Seems to belong there.

---
+++ qsvc/test/test.go
@@ -0,0 +1,254 @@
+// Package test contains standard testing routines for exercising various backends in similar ways.
+package test
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/google/uuid"
+	"github.com/shiblon/entroq"
+	"github.com/shiblon/entroq/qsvc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/test/bufconn"
+
+	pb "github.com/shiblon/entroq/qsvc/proto"
+)
+
+const bufSize = 1 << 20
+
+// Conner returns a grpc connection.
+type Dialer func(ctx context.Context) (*grpc.ClientConn, error)
+
+// StartService starts an in-memory gRPC network service and returns a function for creating client connections to it.
+func StartService(ctx context.Context, opener entroq.BackendOpener) (*grpc.Server, Dialer, error) {
+	lis := bufconn.Listen(bufSize)
+	svc, err := qsvc.New(ctx, opener)
+	if err != nil {
+		return nil, nil, fmt.Errorf("start service: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterEntroQServer(s, svc)
+	go s.Serve(lis)
+
+	return s, func(ctx context.Context) (*grpc.ClientConn, error) {
+		return grpc.DialContext(ctx, "bufnet",
+			grpc.WithInsecure(),
+			grpc.WithDialer(func(string, time.Duration) (net.Conn, error) {
+				return lis.Dial()
+			}))
+	}, nil
+}
+
+// SimpleSequence tests some basic functionality of a task manager, over gRPC.
+func SimpleSequence(ctx context.Context, t *testing.T, client pb.EntroQClient) {
+	t.Helper()
+
+	myID := uuid.New()
+	nowMS := time.Now().UnixNano() / int64(time.Millisecond)
+
+	sleep := func(d time.Duration) {
+		select {
+		case <-time.After(d):
+		case <-ctx.Done():
+			log.Fatalf("Context canceled")
+		}
+	}
+
+	const queue = "/test/TryClaim"
+
+	// Claim from empty queue.
+	claimResp, err := client.TryClaim(ctx, &pb.ClaimRequest{
+		ClaimantId: myID.String(),
+		Queue:      queue,
+		DurationMs: 100,
+	})
+	if err != nil {
+		t.Fatalf("Got unexpected error for claiming from an empty queue: %v", err)
+	}
+	if claimResp.Task != nil {
+		t.Fatalf("Got unexpected non-nil claim response from empty queue:\n%v", proto.MarshalTextString(claimResp))
+	}
+
+	dataFromTask := func(task *pb.Task) *pb.TaskData {
+		td := &pb.TaskData{
+			Queue: task.Queue,
+			AtMs:  task.AtMs,
+			Value: make([]byte, len(task.Value)),
+		}
+		copy(td.Value, task.Value)
+		return td
+	}
+
+	insTask1 := &pb.Task{
+		Queue:      queue,
+		AtMs:       nowMS,
+		Value:      []byte("hello"),
+		ClaimantId: myID.String(),
+	}
+
+	insTask2 := &pb.Task{
+		Queue:      queue,
+		AtMs:       nowMS + 100,
+		Value:      []byte("there"),
+		ClaimantId: myID.String(),
+	}
+
+	// Insert two tasks.
+	modWant := &pb.ModifyResponse{Inserted: []*pb.Task{insTask1, insTask2}}
+	modGot, err := client.Modify(ctx, &pb.ModifyRequest{
+		ClaimantId: myID.String(),
+		Inserts:    []*pb.TaskData{dataFromTask(insTask1), dataFromTask(insTask2)},
+	})
+	if err != nil {
+		t.Fatalf("Got unexpected error inserting two tasks: %v", err)
+	}
+	if err := equalModify(modWant, modGot, 0); err != nil {
+		t.Fatal(err)
+	}
+	// Also check that their arrival times are 100 ms apart as expected:
+	if at0, at1 := modGot.Inserted[0].AtMs, modGot.Inserted[1].AtMs; at1-at0 != 100 {
+		t.Fatalf("Wanted At difference to be %d, got %d: protos\nWant\n%v\nGot\n%v",
+			100, at1-at0, proto.MarshalTextString(modWant), proto.MarshalTextString(modGot))
+	}
+
+	// Get queues.
+	queuesWant := &pb.QueuesResponse{
+		Queues: []*pb.QueueStats{
+			{Name: queue,
+				NumTasks: 2,
+			},
+		},
+	}
+	queuesGot, err := client.Queues(ctx, &pb.QueuesRequest{})
+	if err != nil {
+		t.Fatalf("Getting queues failed: %v", err)
+	}
+	if !proto.Equal(queuesWant, queuesGot) {
+		t.Fatalf("Wanted queues response\n%v\nGot\n%v", proto.MarshalTextString(queuesWant), proto.MarshalTextString(queuesGot))
+	}
+
+	// Get all tasks.
+	tasksGot, err := client.Tasks(ctx, &pb.TasksRequest{Queue: queue})
+	if err != nil {
+		t.Fatalf("Tasks call failed after insertions: %v", err)
+	}
+	if len(tasksGot.Tasks) != 2 {
+		t.Fatalf("Didn't find two tasks when expected: %v", proto.MarshalTextString(tasksGot))
+	}
+	if err := equalTasks(insTask1, tasksGot.Tasks[0], 0); err != nil {
+		t.Fatal(err)
+	}
+	if err := equalTasks(insTask2, tasksGot.Tasks[1], 0); err != nil {
+		t.Fatal(err)
+	}
+
+	// Claim ready task.
+	claimResp, err = client.TryClaim(ctx, &pb.ClaimRequest{
+		ClaimantId: myID.String(),
+		Queue:      queue,
+		DurationMs: 10000,
+	})
+	if err != nil {
+		t.Fatalf("Got unexpected error for claiming from a queue with one ready task: %v", err)
+	}
+	if err := equalTasks(insTask1, claimResp.Task, 1); err != nil {
+		t.Fatalf("Tasks were not as expected when claiming from a queue with one ready task: %v", err)
+	}
+	// Also check that the arrival time is well into the future.
+	if got, lowerBound := claimResp.Task.AtMs, nowMS+10000; got < lowerBound {
+		t.Fatalf("Claiming a task, expected arrival time of at least %d, got %d", lowerBound, got)
+	}
+	if got, upperBound := claimResp.Task.AtMs, nowMS+12500; got > upperBound {
+		t.Fatalf("Expected arrival time at most %d, got %d", upperBound, got)
+	}
+	if claimResp.Task == nil {
+		t.Fatalf("Got unexpected nil claim response from queue with one ready task:\n%v", proto.MarshalTextString(claimResp))
+	}
+
+	// Claim not ready task.
+	claimResp, err = client.TryClaim(ctx, &pb.ClaimRequest{
+		ClaimantId: myID.String(),
+		Queue:      queue,
+		DurationMs: 10000,
+	})
+	if err != nil {
+		t.Fatalf("Got unexpected error for claiming from a queue with no ready tasks: %v", err)
+	}
+	if claimResp.Task != nil {
+		t.Fatalf("Got unexpected non-nil claim response from a queue with no ready tasks:\n%v", proto.MarshalTextString(claimResp))
+	}
+
+	// Make sure the next claim will work.
+	sleep(100 * time.Millisecond)
+	claimResp, err = client.TryClaim(ctx, &pb.ClaimRequest{
+		ClaimantId: myID.String(),
+		Queue:      queue,
+		DurationMs: 5000,
+	})
+	if err != nil {
+		t.Fatalf("Got unexpected error for claiming from a queue with no ready tasks: %v", err)
+	}
+	if err := equalTasks(insTask2, claimResp.Task, 1); err != nil {
+		t.Fatalf("Tasks were not as expected when claiming second task from queue: %v", err)
+	}
+	if got, lowerBound := claimResp.Task.AtMs, nowMS+5000; got < lowerBound {
+		t.Fatalf("Expected arrival time at least %d, got %d", lowerBound, got)
+	}
+	if got, upperBound := claimResp.Task.AtMs, nowMS+7500; got > upperBound {
+		t.Fatalf("Expected arrival time at most %d, got %d", upperBound, got)
+	}
+}
+
+func equalProto(want, got proto.Message) error {
+	if !proto.Equal(want, got) {
+		return fmt.Errorf("Wanted:\n%vGot:\n%v", proto.MarshalTextString(want), proto.MarshalTextString(got))
+	}
+	return nil
+}
+
+func equalTasks(want, got proto.Message, versionChange int) error {
+	ta, tb := want.(*pb.Task), got.(*pb.Task)
+	if (ta == nil) != (tb == nil) {
+		return fmt.Errorf("Wanted equal tasks, but one is nil and one is not: want\n%v\ngot\n%v", proto.MarshalTextString(want), proto.MarshalTextString(got))
+	}
+	if ta == nil {
+		return nil
+	}
+	if ta.Queue != tb.Queue ||
+		ta.Version+int32(versionChange) != tb.Version ||
+		ta.ClaimantId != tb.ClaimantId ||
+		!bytes.Equal(ta.Value, tb.Value) {
+		return fmt.Errorf("Wanted (ignoring id, at_ms, created_ms, and modified_ms):\n%vGot:\n%v",
+			proto.MarshalTextString(want), proto.MarshalTextString(got))
+	}
+	return nil
+}
+
+func equalModify(want, got proto.Message, modVersionChange int) error {
+	rWant, rGot := want.(*pb.ModifyResponse), got.(*pb.ModifyResponse)
+	if w, g := len(rWant.Inserted), len(rGot.Inserted); w != g {
+		return fmt.Errorf("Wanted %d inserted, got %d", w, g)
+	}
+	if w, g := len(rWant.Changed), len(rGot.Changed); w != g {
+		return fmt.Errorf("Wanted %d changed, got %d", w, g)
+	}
+	for i, insWant := range rWant.Inserted {
+		insGot := rGot.Inserted[i]
+		if err := equalTasks(insWant, insGot, 0); err != nil {
+			return fmt.Errorf("in insertion:\n%v", err)
+		}
+	}
+	for i, chgWant := range rWant.Changed {
+		chgGot := rGot.Changed[i]
+		if err := equalTasks(chgWant, chgGot, modVersionChange); err != nil {
+			return fmt.Errorf("in change:\n%v", err)
+		}
+	}
+	return nil
+}
