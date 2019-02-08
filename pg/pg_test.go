3ede9bce0042f0dbb44dcfb9bb496a8656314460
Chris Monson <shiblon@gmail.com>
2019-02-08 13:57:46 -0500

    Restored larger check tests.

--- pg/pg_test.go
+++ pg/pg_test.go
@@ -95,10 +95,10 @@ func TestMapReduce_checkSmall(t *testing.T) {
 	defer stop()
 
 	config := &quick.Config{
-		MaxCount: 2,
+		MaxCount: 3,
 		Values: func(values []reflect.Value, rand *rand.Rand) {
-			values[0] = reflect.ValueOf(rand.Intn(100) + 100)
-			values[1] = reflect.ValueOf(rand.Intn(20) + 1)
+			values[0] = reflect.ValueOf(rand.Intn(200) + 100)
+			values[1] = reflect.ValueOf(rand.Intn(20) + 10)
 			values[2] = reflect.ValueOf(rand.Intn(4) + 1)
 		},
 	}
