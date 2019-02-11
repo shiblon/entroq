ef141c06e31e2d5642fab2408f013bc9f0a749db
Chris Monson <shiblon@gmail.com>
2019-02-11 00:51:50 -0500

    Notify in goroutine, removing hacky sleep cycles.
    Also actually move mrcheck test into mem package.
    
    Also update postgres serialization retries to 7.

--- mem/mem_test.go
+++ mem/mem_test.go
@@ -2,8 +2,13 @@ package mem
 
 import (
 	"context"
+	"math/rand"
+	"reflect"
 	"testing"
+	"testing/quick"
 
+	"github.com/shiblon/entroq"
+	"github.com/shiblon/entroq/contrib/mrtest"
 	"github.com/shiblon/entroq/qsvc/qtest"
 )
 
@@ -41,4 +46,27 @@ func TestQueueMatch(t *testing.T) {
 	defer stop()
 
 	qtest.QueueMatch(ctx, t, client, "")
+}
+
+func TestMapReduce_checkLarge(t *testing.T) {
+	config := &quick.Config{
+		MaxCount: 5,
+		Values: func(values []reflect.Value, rand *rand.Rand) {
+			values[0] = reflect.ValueOf(rand.Intn(5000) + 5000)
+			values[1] = reflect.ValueOf(rand.Intn(100) + 1)
+			values[2] = reflect.ValueOf(rand.Intn(20) + 1)
+		},
+	}
+
+	ctx := context.Background()
+	check := func(ndocs, nm, nr int) bool {
+		client, err := entroq.New(ctx, Opener())
+		if err != nil {
+			t.Fatalf("Open mem client: %v", err)
+		}
+		return mrtest.MRCheck(ctx, client, ndocs, nm, nr)
+	}
+	if err := quick.Check(check, config); err != nil {
+		t.Fatal(err)
+	}
 }
