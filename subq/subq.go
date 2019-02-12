fdfba80824e620de084d3de81e321979790c65c0
Chris Monson <shiblon@gmail.com>
2019-02-11 20:19:24 -0500

    Simplified layout of subq to make reasoning easier.

--- subq/subq.go
+++ subq/subq.go
@@ -83,7 +83,8 @@ func (s *SubQ) Notify(q string) {
 	}
 
 	// Do this asynchronously - we don't need nor want to wait around for the
-	// cond function to run and the value to be picked up.
+	// condition function to run and the value to be picked up. Note that the lock
+	// is not held in this goroutine.
 	go func() {
 		// Because the Wait function lets go of the mutex just before selecting on
 		// its channel, there is a case where we can fail to notify even though
@@ -105,23 +106,72 @@ func (s *SubQ) Notify(q string) {
 	}()
 }
 
+// makeDefaultCondition creates a condition function that returns false once, then true.
+// This will cause the Wait function to wait exactly one time through its
+// polling loop.
+func makeDefaultCondition() func() bool {
+	var done bool
+	return func() bool {
+		val := done
+		done = true
+		return val
+	}
+}
+
+// qInfo returns the requested queue wait channel and reservation mechanism,
+// creating one if not present. If it creates one, it also starts up a garbage
+// collection routine for it.
+func (s *SubQ) qInfo(q string) *sub {
+	defer un(lock(s))
+
+	qs := s.qs
+
+	// If there isn't any sync info for this queue, create it and add this
+	// waiter. Otherwise just add the waiter; the sync info is there already.
+	if qs[q] != nil {
+		qs[q].Reserve()
+		return qs[q]
+	}
+
+	qs[q] = &sub{ch: make(chan string)}
+	qs[q].Reserve()
+
+	// Start up a watchdog that deletes when there are no more listeners.
+	// Only do this when creating a new pInfo entry.
+	go func() {
+		for {
+			s.Lock()
+			if !qs[q].Reserved() {
+				delete(qs, q)
+			}
+			if qs[q] == nil {
+				s.Unlock()
+				return
+			}
+			s.Unlock()
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	return qs[q]
+}
+
 // Wait waits on the given queue until something is notified on it or the
 // context expires, whichever comes first. This is the basic behavior when
-// pollWait is 0 and cond is nil.
+// pollWait is 0 and condition is nil.
 //
-// If cond is not nil, it is called immediately. If it returns true, then the
+// If condition is not nil, it is called immediately. If it returns true, then the
 // wait is satisfied and the function exits with a nil error. If it returns
 // false, then the function begins to wait for either a notification, a context
 // cancelation, or the expiration of pollWait.
 //
-// Note that cond should execute quickly and not block. It should test the
+// Note that condition should execute quickly and not block. It should test the
 // condition as fast as it can and return. Otherwise "Notify" might busy-wait
 // for a while, which is obviously not good.
 //
-// When pollWait expires or a suitable notification arrives, cond is called
+// When pollWait expires or a suitable notification arrives, condition is called
 // again, and the above process repeats.
 //
-// If pollWait is 0, the only way to check cond again is if the channel is
+// If pollWait is 0, the only way to check condition again is if the channel is
 // notified. Otherwise Wait terminates with an error.
 //
 // This implementation allows you to attempt a polling operation, then wait for
@@ -129,80 +179,33 @@ func (s *SubQ) Notify(q string) {
 // in case you got scooped by another process, repeating until something is
 // truly available.
 //
-// Note that cond is called directly from this function, so if it needs a
+// Note that condition is called directly from this function, so if it needs a
 // context, it can simply close over the same one passed in here.
-func (s *SubQ) Wait(ctx context.Context, q string, pollWait time.Duration, cond func() bool) error {
-	if cond == nil {
-		// A nil cond function means, basically, "just wait once and tell me what happened".
-		// The first time it's called, it returns false, then true ever after,
-		// giving use the wait behavior we want below. Wrapped in an
-		// immediately-called closure to avoid scope leakage.
-		cond = func() func() bool {
-			var done bool
-			return func() bool {
-				defer func() {
-					done = true
-				}()
-				return done
-			}
-		}()
+func (s *SubQ) Wait(ctx context.Context, q string, pollWait time.Duration, condition func() bool) error {
+	if condition == nil {
+		condition = makeDefaultCondition()
 	}
 
-	var qInfo *sub
-	func() {
-		defer un(lock(s))
-
-		qs := s.qs
-
-		// If there isn't any sync info for this queue, create it and add this
-		// waiter. Otherwise just add the waiter; the sync info is there already.
-		qInfo = qs[q]
-		if qInfo != nil {
-			qInfo.Reserve()
-			return
-		}
-
-		qInfo = &sub{ch: make(chan string)}
-		qInfo.Reserve()
-		qs[q] = qInfo
-
-		// Start up a watchdog that deletes when there are no more listeners.
-		// Only do this when creating a new pInfo entry.
-		go func() {
-			for {
-				s.Lock()
-				if !qs[q].Reserved() {
-					delete(qs, q)
-				}
-				if qs[q] == nil {
-					s.Unlock()
-					return
-				}
-				s.Unlock()
-				time.Sleep(1 * time.Second)
-			}
-		}()
-	}()
-	defer qInfo.Release()
-
-	for {
-		if cond() {
-			return nil
-		}
-
-		// If not waiting, after is nil, so never satisfied below.
-		var after <-chan time.Time
+	wait := func() <-chan time.Time {
 		if pollWait > 0 {
-			after = time.After(pollWait)
+			return time.After(pollWait)
 		}
+		// Zero wait means wait forever.
+		return nil
+	}
 
+	qi := s.qInfo(q)
+	defer qi.Release()
+
+	for !condition() {
 		select {
-		case <-after:
+		case <-wait():
 			// Go around again, even though not signaled.
-		case <-qInfo.Ch():
-			// Got a value, go around again to run cond to see if it's still there.
+		case <-qi.Ch():
+			// Got a value, go around again to run condition to see if it's still there.
 		case <-ctx.Done():
 			return ctx.Err()
 		}
 	}
+	return nil
 }
