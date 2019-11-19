5b2ff4ccecf7e133bfa708165dd8c749db51659b
Chris Monson <shiblon@gmail.com>
2019-11-19 11:13:31 -0500

    Doc fix, retries added for task deletion.

--- cmd/eqc/cmd/rm.go
+++ cmd/eqc/cmd/rm.go
@@ -19,6 +19,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 
 	"entrogo.com/entroq"
 	"github.com/google/uuid"
@@ -30,6 +31,7 @@ var (
 	flagRmQueue   string
 	flagRmQueueTo string
 	flagRmVal     string
+	flagRmRetries int
 )
 
 func init() {
@@ -37,6 +39,8 @@ func init() {
 
 	rmCmd.Flags().StringVarP(&flagRmID, "task", "t", "", "Task ID to remove. Note that this will remove whatever version of the task ID it finds. Use with care. Required.")
 	rmCmd.MarkFlagRequired("task")
+
+	rmCmd.Flags().IntVarP(&flagRmRetries, "retries", "r", 5, "Retries (in case the task is claimed)")
 
 	rmCmd.Flags().StringVarP(&flagRmQueue, "queue", "q", "", "Queue containing the task to remove. Required.")
 	rmCmd.MarkFlagRequired("queue")
@@ -51,26 +55,37 @@ var rmCmd = &cobra.Command{
 		if err != nil {
 			log.Fatalf("Error parsing task ID: %v", err)
 		}
-		tasks, err := eq.Tasks(context.Background(), flagRmQueue, entroq.WithTaskID(id))
-		if err != nil {
-			log.Fatalf("Error getting task ID %q", id)
-		}
-		if len(tasks) < 1 {
-			log.Fatalf("Could not find task ID %q", id)
-		}
-		if len(tasks) > 1 {
-			log.Fatalf("Too many tasks returned: %v", tasks)
-		}
+		ctx := context.Background()
+		var delErr error
+		for i := 0; i < flagRmRetries; i++ {
+			log.Printf("Attempt %d/%d to remove %v", i+1, flagRmRetries, id)
+			tasks, err := eq.Tasks(ctx, flagRmQueue, entroq.WithTaskID(id))
+			if err != nil {
+				log.Fatalf("Error getting task ID %v", id)
+			}
+			if len(tasks) < 1 {
+				log.Fatalf("Could not find task ID %v", id)
+			}
+			if len(tasks) > 1 {
+				log.Fatalf("Too many tasks returned: %v", tasks)
+			}
 
-		_, mod, err := eq.Modify(context.Background(), tasks[0].AsDeletion())
-		if err != nil {
-			log.Fatalf("Could not remove task %v: %v", id, err)
-		}
+			_, mod, err := eq.Modify(ctx, tasks[0].AsDeletion())
+			if err != nil {
+				log.Printf("Try %d/%d - could not remove task %v: %v", i+1, flagRmRetries, id, err)
+				delErr = err
+				time.Sleep(3 * time.Second)
+				continue
+			}
 
-		b, err := json.MarshalIndent(mod, "", "\t")
-		if err != nil {
-			log.Fatalf("JSON marshal: %v", err)
+			b, err := json.MarshalIndent(mod, "", "\t")
+			if err != nil {
+				log.Fatalf("JSON marshal: %v", err)
+			}
+
+			fmt.Println(string(b))
+			return
 		}
-		fmt.Println(string(b))
+		log.Fatalf("Could not delete task %v: %v", id, delErr)
 	},
 }
