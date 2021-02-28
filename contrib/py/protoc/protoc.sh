bdf62cadddc35eb7bc9fce5036c51f1495f44681
Chris Monson <shiblon@gmail.com>
2021-02-28 18:21:39 -0500

    Moved python proto build to a docker container.

---
+++ contrib/py/protoc/protoc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+imgname=entroq-pyprotoc:local
+imgid="$(docker image ls "$imgname")"
+if [[ "$imgid" == "" ]]; then
+  docker build -t "$imgname" .
+fi
+
+target_dir=contrib/py/entroq
+
+cd ../../..
+echo "Now in directory $PWD, running docker"
+
+docker run --rm -it -v "$PWD":/src "$imgname" \
+  bash -c "python -m grpc_tools.protoc -Iproto --python_out="$target_dir"/ --grpc_python_out="$target_dir"/ proto/*.proto"
+
+# Fix up the stupid non-relative import that grpc output makes.
+sed -i'' -e 's/^import entroq_pb2 as/from . import entroq_pb2 as/' "$target_dir/entroq_pb2_grpc.py"
+
