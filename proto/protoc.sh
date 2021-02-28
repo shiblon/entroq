70a5d641278f5f50c4950bd91c7cff60ef87a725
Chris Monson <shiblon@gmail.com>
2021-02-28 17:53:40 -0500

    Make go proto compilation happen inside a docker container.

--- proto/protoc.sh
+++ proto/protoc.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+imgname=entroq-goprotoc:local
+
 cd "$(dirname "$0")"
-protoc -I. --go_out=. --go-grpc_out=. ./*.proto
+
+imgid="$(docker image ls -q "$imgname")"
+if [[ "$imgid" == "" ]]; then
+  docker build -t "$imgname" .
+fi
+
+docker run --rm -it -v "$PWD":/src "$imgname" \
+  bash -c "protoc -I/src --go_out=/src --go-grpc_out=/src /src/*.proto"
