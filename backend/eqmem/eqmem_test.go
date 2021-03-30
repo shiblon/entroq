cbbf7be9c9a244f536ccf1781ba8cc738710e7d5
Chris Monson <shiblon@gmail.com>
2021-03-30 15:33:26 -0600

    Added test for claim changes, fixed bug with missing claimants in journal output.

--- backend/eqmem/eqmem_test.go
+++ backend/eqmem/eqmem_test.go
@@ -3,13 +3,16 @@ package eqmem
 import (
 	"context"
 	"math/rand"
+	"os"
 	"reflect"
 	"testing"
 	"testing/quick"
+	"time"
 
 	"entrogo.com/entroq"
 	"entrogo.com/entroq/contrib/mrtest"
 	"entrogo.com/entroq/qsvc/qtest"
+	"github.com/google/go-cmp/cmp"
 )
 
 func RunQTest(t *testing.T, tester qtest.Tester) {
@@ -145,5 +148,66 @@ func TestEQMemMapReduce_checkHuge(t *testing.T) {
 	}
 	if err := quick.Check(check, config); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestEQMemJournalClaim(t *testing.T) {
+	journalDir, err := os.MkdirTemp("", "eqjournal-")
+	if err != nil {
+		t.Fatalf("Error opening temp dir for journal: %v", err)
+	}
+	defer os.RemoveAll(journalDir)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	eq, err := entroq.New(ctx, Opener(WithJournal(journalDir)))
+	if err != nil {
+		t.Fatalf("Error opening client at dir %q: %v", journalDir, err)
+	}
+
+	if _, _, err := eq.Modify(ctx,
+		entroq.InsertingInto("/queue/of/tasks", entroq.WithValue([]byte("hey"))),
+		entroq.InsertingInto("/queue/of/others", entroq.WithValue([]byte("other"))),
+	); err != nil {
+		t.Fatalf("Error adding task: %v", err)
+	}
+
+	if _, err := eq.Claim(ctx, entroq.From("/queue/of/tasks")); err != nil {
+		t.Fatalf("Failed to claim from /queue/of/tasks: %v", err)
+	}
+
+	// Close and reopen, see that everything is still there.
+	eq.Close()
+	eq = nil
+
+	if eq, err = entroq.New(ctx, Opener(WithJournal(journalDir))); err != nil {
+		t.Fatalf("Error reopening client at dir %q: %v", journalDir, err)
+	}
+
+	expect := map[string]*entroq.QueueStat{
+		"/queue/of/tasks": &entroq.QueueStat{
+			Name:      "/queue/of/tasks",
+			Claimed:   1,
+			Available: 0,
+			Size:      1,
+			MaxClaims: 1,
+		},
+		"/queue/of/others": &entroq.QueueStat{
+			Name:      "/queue/of/others",
+			Claimed:   0,
+			Available: 1,
+			Size:      1,
+			MaxClaims: 0,
+		},
+	}
+
+	stats, err := eq.QueueStats(ctx)
+	if err != nil {
+		t.Fatalf("Queue stats: %v", err)
+	}
+
+	if diff := cmp.Diff(expect, stats); diff != "" {
+		t.Errorf("Unexpected diff (-want +got):\n%v", diff)
 	}
 }
