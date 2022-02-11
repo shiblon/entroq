497c1e469876601d14d05d857baf54d81365dbc4
Chris Monson <shiblon@gmail.com>
2022-02-11 14:24:49 -0500

    Update proto version, address security issue.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
+from setuptools import setup, find_packages
 import os
 import sys
+
 
 def iter_protos(parent=None):
     for root, _, files in os.walk('proto'):
@@ -8,7 +10,6 @@ def iter_protos(parent=None):
         dest = root if not parent else os.path.join(parent, root)
         yield dest, [os.path.join(root, f) for f in files]
 
-from setuptools import setup, find_packages
 
 pkg_name = 'entroq'
 
@@ -29,7 +30,7 @@ setup(name=pkg_name,
           'grpcio-status==1.25.0',
           'grpcio-tools==1.25.0',
           'grpcio_health_checking==1.25.0',
-          'protobuf==3.10.0',
+          'protobuf==3.15.0',
           'Click==7.0',
       ],
       data_files=list(iter_protos(pkg_name)),
