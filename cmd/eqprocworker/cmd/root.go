da6e464fc0241ea1b1ba433d6d25b5256b54d289
Chris Monson <shiblon@gmail.com>
2026-04-08 16:45:02 -0400

    moved procworker to pkg/workers

--- cmd/eqprocworker/cmd/root.go
+++ cmd/eqprocworker/cmd/root.go
@@ -24,7 +24,7 @@ import (
 
 	"github.com/shiblon/entroq"
 	"github.com/shiblon/entroq/backend/eqgrpc"
-	"github.com/shiblon/entroq/pkg/procworker"
+	"github.com/shiblon/entroq/pkg/workers/proc"
 	"github.com/shiblon/entroq/pkg/worker"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
