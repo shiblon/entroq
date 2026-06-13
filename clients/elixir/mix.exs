2e9d796bc22d37b8c1a55067fe3df157970a7ac3
Chris Monson <chris@abundantsecurity.co>
2026-06-13 18:00:25 +0000

    docs(clients): enable ExDoc for Elixir client
    Add ExDoc as a dev-only dependency and configure generated docs to use the Elixir client README as the main page with source links back to the monorepo subdirectory.

--- clients/elixir/mix.exs
+++ clients/elixir/mix.exs
@@ -9,6 +9,7 @@ defmodule EntroQ.MixProject do
       start_permanent: Mix.env() == :prod,
       description: "Elixir client for the EntroQ task queue.",
       package: package(),
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -21,7 +22,8 @@ defmodule EntroQ.MixProject do
 
   defp deps do
     [
-      {:jason, "~> 1.4"}
+      {:jason, "~> 1.4"},
+      {:ex_doc, "~> 0.35", only: :dev, runtime: false}
     ]
   end
 
@@ -29,6 +31,17 @@ defmodule EntroQ.MixProject do
     [
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/shiblon/entroq"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"],
+      source_url: "https://github.com/shiblon/entroq",
+      source_ref: "develop",
+      source_url_pattern:
+        "https://github.com/shiblon/entroq/blob/develop/clients/elixir/%{path}#L%{line}"
     ]
   end
 end
